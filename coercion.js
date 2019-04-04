08d3b2ff13d17825511955a16183cd07f8e2a452
nendonerd <wang3721.2011@gmail.com>
2019-04-04 17:40:01 +0800

    roll back to original coercion script

---
+++ coercion.js
@@ -0,0 +1,108 @@
+function coerceCompare(a, b) {
+  let typeA = typeOf(a) //?
+  let typeB = typeOf(b) //?
+  let set = new Set([typeA, typeB])
+  let data = {a: a, b: b}
+
+  if (set.size === 1) {return a === b}
+  if (set.has('Array') && set.has('Object')) {
+    render(data, String(a === b))
+    return a === b
+  }
+
+  if (set.has('Boolean')) {
+    let boo = find(data, 'Boolean')
+    data[boo] = Number(data[boo])
+    render(data)
+  }
+  if (set.has('String') && set.has('Number')) {
+    let str = find(data, 'String')
+    data[str] = Number(data[str])
+    render(data)
+  }
+  if (set.has('Array')) {
+    let arr = find(data, 'Array')
+    data[arr] = toPrimitive(data[arr])
+    render(data)
+  }
+  if (set.has('Object')) {
+    let obj = find(data, 'Object')
+    data[obj] = toPrimitive(data[obj])
+    render(data)
+  }
+  if (set.has('Undefined') && set.has('Null')) {
+    render(data, 'true')
+    return true
+  }
+  if (set.has('Undefined') || set.has('Null')) {
+    render(data, 'false')
+    return false
+  }
+
+  a = data.a
+  b = data.b
+
+  return coerceCompare(a, b)
+}
+
+function find({a, b}, type) {
+  if (typeOf(a) === type) {return 'a'}
+  else {return 'b'}
+}
+
+function toPrimitive(x) {
+  if (typeOf(x.toString()) !== 'Array' && typeOf(x.toString()) !== 'Object') {
+    return x.toString()
+  } else if (typeOf(x.valueOf()) !== 'Array' && typeOf(x.valueOf()) !== 'Object'){
+    return x.valueOf()
+  } else {
+    return undefined
+  }
+}
+
+function typeOf(x) {
+  let type = Object.prototype.toString.call(x)
+  return type.substring(8, type.length-1)
+}
+
+
+const render = (() => {
+  if (location.href !== 'about:blank') { // for quokka compatibility
+    return renderInBrowser
+  } else {
+    return renderInNode
+  }
+})()
+
+// ENV: Node
+function renderInNode({ a, b }, end) {
+  let typeA = Type(a) //?
+  let typeB = Type(b)
+  if (end !== undefined) {
+    return String(end) //?
+  } else {
+    return ('a: ' + a + typeA + ' | ' + 'b: ' + b + typeB)
+  }
+}
+
+// ENV: Browser
+function renderInBrowser({ a, b }, end) {
+
+  let typeA = typeOf(a) //?
+  let typeB = typeOf(b)
+  let msg
+
+  if (end !== undefined) {
+    msg = String(end)
+  } else {
+    msg = ('a: ' + typeA + ' | ' + 'b: ' + typeB)
+  }
+
+  let result = document.querySelector('.result')
+
+  let step = document.createElement('li')
+  step.textContent = msg
+
+  result.append(step)
+
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+## How to deploy:
+
 install_docker () {
   curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh get-docker.sh
@@ -8,7 +10,12 @@ type docker || install_docker
 
 type git || sudo apt-get install git -y
 
-IMAGE_NAME=${$1:-instance}
+IMAGE_NAME=${1:-coercion}
+GIT_REPO=${2:-...}
+
+mkdir $IMAGE_NAME
+cd $IMAGE_NAME
+git clone $GIT_REPO .
 
 sudo docker build -t IMAGE_NAME .
 sudo docker run -d -p 80:80 IMAGE_NAME
