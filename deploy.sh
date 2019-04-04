16f082508542a259b7d710c7534b776d829c4734
nendonerd <wang3721.2011@gmail.com>
2019-04-04 19:10:15 +0800

    adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+install_docker () {
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  sudo usermod -aG docker $(users)
+}
+
+type docker || install_docker
+
+type git || sudo apt-get install git -y
+
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+sudo service docker restart
+
+IMAGE_NAME=${1:-coercion}
+GIT_REPO=${2:-'https://github.com/nendonerd/coercion-visual-demo.git'}
+
+mkdir $IMAGE_NAME
+cd $IMAGE_NAME
+git clone $GIT_REPO .
+
+sudo docker build -t $IMAGE_NAME .
+sudo docker run -d -p 80:80 $IMAGE_NAME
--- readme.md
+++ readme.md
@@ -10,12 +10,16 @@ type docker || install_docker
 
 type git || sudo apt-get install git -y
 
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+sudo service docker restart
+
 IMAGE_NAME=${1:-coercion}
-GIT_REPO=${2:-...}
+GIT_REPO=${2:-'https://github.com/nendonerd/coercion-visual-demo.git'}
 
 mkdir $IMAGE_NAME
 cd $IMAGE_NAME
 git clone $GIT_REPO .
 
-sudo docker build -t IMAGE_NAME .
-sudo docker run -d -p 80:80 IMAGE_NAME
+sudo docker build -t $IMAGE_NAME .
+sudo docker run -d -p 80:80 $IMAGE_NAME
